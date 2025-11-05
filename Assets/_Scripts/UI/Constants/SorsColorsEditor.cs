60004d5755360b072a810bc71975c07754dc37b3
Matthias W <m.wuest53@gmail.com>
2025-11-05 12:40:15 +0100

    Fix/build: Comment custom editor drawer for Steam build.

--- Assets/_Scripts/UI/Constants/SorsColorsEditor.cs
+++ Assets/_Scripts/UI/Constants/SorsColorsEditor.cs
@@ -1,184 +1,184 @@
-#if UNITY_EDITOR
+// #if UNITY_EDITOR
 
-using UnityEngine;
-using UnityEditor;
-using UnityEditor.Presets;
+// using UnityEngine;
+// using UnityEditor;
+// using UnityEditor.Presets;
 
-[CustomEditor(typeof(SorsColors))]
-[System.Serializable]
-public class SorsColorsEditor : Editor
-{
-    public const string baseResourcePath = "ColorDefinitions/";
-    public const string defaultEditorSkin = "SorsCustomEditorSkin";
-    public const string defaultPreset = "SorsColorsPreset";
-    public const string defaultColorsEditor = "Sors Colors";
+// [CustomEditor(typeof(SorsColors))]
+// [System.Serializable]
+// public class SorsColorsEditor : Editor
+// {
+//     public const string baseResourcePath = "ColorDefinitions/";
+//     public const string defaultEditorSkin = "SorsCustomEditorSkin";
+//     public const string defaultPreset = "SorsColorsPreset";
+//     public const string defaultColorsEditor = "Sors Colors";
 
-    GUISkin customSkin;
-    protected static float foldoutItemSpace = 2;
-    protected static float foldoutTopSpace = 5;
-    protected static float foldoutBottomSpace = 2;
-    protected static bool showPlayers = true;
-    protected static bool showTypes = true;
-    protected static bool showStats = true;
-    protected static bool showUIColors = true;
-    protected static bool showHighlights = true;
+//     GUISkin customSkin;
+//     protected static float foldoutItemSpace = 2;
+//     protected static float foldoutTopSpace = 5;
+//     protected static float foldoutBottomSpace = 2;
+//     protected static bool showPlayers = true;
+//     protected static bool showTypes = true;
+//     protected static bool showStats = true;
+//     protected static bool showUIColors = true;
+//     protected static bool showHighlights = true;
 
-    void OnEnable()
-    {
-        customSkin = (GUISkin)Resources.Load(baseResourcePath + defaultEditorSkin);
-    }
+//     void OnEnable()
+//     {
+//         customSkin = (GUISkin)Resources.Load(baseResourcePath + defaultEditorSkin);
+//     }
 
-    public override void OnInspectorGUI()
-    {
-        // Foldout style
-        GUIStyle foldoutStyle = customSkin.FindStyle("UIM Foldout");
+//     public override void OnInspectorGUI()
+//     {
+//         // Foldout style
+//         GUIStyle foldoutStyle = customSkin.FindStyle("UIM Foldout");
 
-        // UIM Header
-        EditorDrawer.DrawHeader(customSkin, "UIM Header", 8);
-        GUILayout.BeginVertical(EditorStyles.helpBox);
+//         // UIM Header
+//         EditorDrawer.DrawHeader(customSkin, "UIM Header", 8);
+//         GUILayout.BeginVertical(EditorStyles.helpBox);
 
-        #region Players       
-        var player = serializedObject.FindProperty("player");
-        var opponent = serializedObject.FindProperty("opponent");
+//         #region Players       
+//         var player = serializedObject.FindProperty("player");
+//         var opponent = serializedObject.FindProperty("opponent");
 
-        GUILayout.Space(foldoutTopSpace);
-        GUILayout.BeginHorizontal();
-        showPlayers = EditorGUILayout.Foldout(showPlayers, "Players", true, foldoutStyle);
-        showPlayers = GUILayout.Toggle(showPlayers, new GUIContent(""), customSkin.FindStyle("Toggle Helper"));
-        GUILayout.EndHorizontal();
-        GUILayout.Space(foldoutBottomSpace);
+//         GUILayout.Space(foldoutTopSpace);
+//         GUILayout.BeginHorizontal();
+//         showPlayers = EditorGUILayout.Foldout(showPlayers, "Players", true, foldoutStyle);
+//         showPlayers = GUILayout.Toggle(showPlayers, new GUIContent(""), customSkin.FindStyle("Toggle Helper"));
+//         GUILayout.EndHorizontal();
+//         GUILayout.Space(foldoutBottomSpace);
 
-        if (showPlayers)
-        {
-            EditorDrawer.DrawProperty(player, customSkin, "Player");
-            EditorDrawer.DrawProperty(opponent, customSkin, "Opponent");
-        }
-        #endregion
+//         if (showPlayers)
+//         {
+//             EditorDrawer.DrawProperty(player, customSkin, "Player");
+//             EditorDrawer.DrawProperty(opponent, customSkin, "Opponent");
+//         }
+//         #endregion
 
-        #region Card Types       
-        var creature = serializedObject.FindProperty("creature");
-        var technology = serializedObject.FindProperty("technology");
-        var money = serializedObject.FindProperty("money");
+//         #region Card Types       
+//         var creature = serializedObject.FindProperty("creature");
+//         var technology = serializedObject.FindProperty("technology");
+//         var money = serializedObject.FindProperty("money");
 
-        GUILayout.Space(foldoutTopSpace);
-        GUILayout.BeginHorizontal();
-        showTypes = EditorGUILayout.Foldout(showTypes, "Card Types", true, foldoutStyle);
-        showTypes = GUILayout.Toggle(showTypes, new GUIContent(""), customSkin.FindStyle("Toggle Helper"));
-        GUILayout.EndHorizontal();
-        GUILayout.Space(foldoutBottomSpace);
+//         GUILayout.Space(foldoutTopSpace);
+//         GUILayout.BeginHorizontal();
+//         showTypes = EditorGUILayout.Foldout(showTypes, "Card Types", true, foldoutStyle);
+//         showTypes = GUILayout.Toggle(showTypes, new GUIContent(""), customSkin.FindStyle("Toggle Helper"));
+//         GUILayout.EndHorizontal();
+//         GUILayout.Space(foldoutBottomSpace);
 
-        if (showTypes)
-        {
-            EditorDrawer.DrawProperty(creature, customSkin, "Creature");
-            EditorDrawer.DrawProperty(technology, customSkin, "Technology");
-            EditorDrawer.DrawProperty(money, customSkin, "Money");
-        }
-        #endregion
+//         if (showTypes)
+//         {
+//             EditorDrawer.DrawProperty(creature, customSkin, "Creature");
+//             EditorDrawer.DrawProperty(technology, customSkin, "Technology");
+//             EditorDrawer.DrawProperty(money, customSkin, "Money");
+//         }
+//         #endregion
 
-        #region Card Stats       
-        var cost = serializedObject.FindProperty("costValue");
-        var attack = serializedObject.FindProperty("attackValue");
-        var health = serializedObject.FindProperty("healthValue");
-        var points = serializedObject.FindProperty("pointsValue");
+//         #region Card Stats       
+//         var cost = serializedObject.FindProperty("costValue");
+//         var attack = serializedObject.FindProperty("attackValue");
+//         var health = serializedObject.FindProperty("healthValue");
+//         var points = serializedObject.FindProperty("pointsValue");
 
-        GUILayout.Space(foldoutTopSpace);
-        GUILayout.BeginHorizontal();
-        showStats = EditorGUILayout.Foldout(showStats, "Card Stats", true, foldoutStyle);
-        showStats = GUILayout.Toggle(showStats, new GUIContent(""), customSkin.FindStyle("Toggle Helper"));
-        GUILayout.EndHorizontal();
-        GUILayout.Space(foldoutBottomSpace);
+//         GUILayout.Space(foldoutTopSpace);
+//         GUILayout.BeginHorizontal();
+//         showStats = EditorGUILayout.Foldout(showStats, "Card Stats", true, foldoutStyle);
+//         showStats = GUILayout.Toggle(showStats, new GUIContent(""), customSkin.FindStyle("Toggle Helper"));
+//         GUILayout.EndHorizontal();
+//         GUILayout.Space(foldoutBottomSpace);
 
-        if (showStats)
-        {
-            EditorDrawer.DrawProperty(cost, customSkin, "Cost");
-            EditorDrawer.DrawProperty(attack, customSkin, "Attack");
-            EditorDrawer.DrawProperty(health, customSkin, "Health");
-            EditorDrawer.DrawProperty(points, customSkin, "Points");
-        }
-        #endregion
+//         if (showStats)
+//         {
+//             EditorDrawer.DrawProperty(cost, customSkin, "Cost");
+//             EditorDrawer.DrawProperty(attack, customSkin, "Attack");
+//             EditorDrawer.DrawProperty(health, customSkin, "Health");
+//             EditorDrawer.DrawProperty(points, customSkin, "Points");
+//         }
+//         #endregion
 
-        #region UI
-        var callToAction = serializedObject.FindProperty("callToAction");
-        var neutral = serializedObject.FindProperty("neutral");
-        var textBackground = serializedObject.FindProperty("textBackground");
+//         #region UI
+//         var callToAction = serializedObject.FindProperty("callToAction");
+//         var neutral = serializedObject.FindProperty("neutral");
+//         var textBackground = serializedObject.FindProperty("textBackground");
 
-        GUILayout.Space(foldoutTopSpace);
-        GUILayout.BeginHorizontal();
-        showUIColors = EditorGUILayout.Foldout(showUIColors, "UI Colors", true, foldoutStyle);
-        showUIColors = GUILayout.Toggle(showUIColors, new GUIContent(""), customSkin.FindStyle("Toggle Helper"));
-        GUILayout.EndHorizontal();
-        GUILayout.Space(foldoutBottomSpace);
+//         GUILayout.Space(foldoutTopSpace);
+//         GUILayout.BeginHorizontal();
+//         showUIColors = EditorGUILayout.Foldout(showUIColors, "UI Colors", true, foldoutStyle);
+//         showUIColors = GUILayout.Toggle(showUIColors, new GUIContent(""), customSkin.FindStyle("Toggle Helper"));
+//         GUILayout.EndHorizontal();
+//         GUILayout.Space(foldoutBottomSpace);
 
-        if (showUIColors)
-        {
-            EditorDrawer.DrawProperty(callToAction, customSkin, "Call to Action");
-            EditorDrawer.DrawProperty(neutral, customSkin, "Neutral");
-            EditorDrawer.DrawProperty(textBackground, customSkin, "Text Background");
-        }
+//         if (showUIColors)
+//         {
+//             EditorDrawer.DrawProperty(callToAction, customSkin, "Call to Action");
+//             EditorDrawer.DrawProperty(neutral, customSkin, "Neutral");
+//             EditorDrawer.DrawProperty(textBackground, customSkin, "Text Background");
+//         }
 
-        #endregion
+//         #endregion
 
-        #region Highlights
-        var defaultHighlight = serializedObject.FindProperty("defaultHighlight");
-        var interactionPositiveHighlight = serializedObject.FindProperty("interactionPositiveHighlight");
-        var interactionNegativeHighlight = serializedObject.FindProperty("interactionNegativeHighlight");
-        var targetHighlight = serializedObject.FindProperty("targetHighlight");
-        var triggerHighlight = serializedObject.FindProperty("triggerHighlight");
-        var abilityHighlight = serializedObject.FindProperty("abilityHighlight");
-        var selectedHighlight = serializedObject.FindProperty("selectedHighlight");
+//         #region Highlights
+//         var defaultHighlight = serializedObject.FindProperty("defaultHighlight");
+//         var interactionPositiveHighlight = serializedObject.FindProperty("interactionPositiveHighlight");
+//         var interactionNegativeHighlight = serializedObject.FindProperty("interactionNegativeHighlight");
+//         var targetHighlight = serializedObject.FindProperty("targetHighlight");
+//         var triggerHighlight = serializedObject.FindProperty("triggerHighlight");
+//         var abilityHighlight = serializedObject.FindProperty("abilityHighlight");
+//         var selectedHighlight = serializedObject.FindProperty("selectedHighlight");
 
-        GUILayout.Space(foldoutTopSpace);
-        GUILayout.BeginHorizontal();
-        showHighlights = EditorGUILayout.Foldout(showHighlights, "Highlights", true, foldoutStyle);
-        showHighlights = GUILayout.Toggle(showHighlights, new GUIContent(""), customSkin.FindStyle("Toggle Helper"));
-        GUILayout.EndHorizontal();
-        GUILayout.Space(foldoutBottomSpace);
+//         GUILayout.Space(foldoutTopSpace);
+//         GUILayout.BeginHorizontal();
+//         showHighlights = EditorGUILayout.Foldout(showHighlights, "Highlights", true, foldoutStyle);
+//         showHighlights = GUILayout.Toggle(showHighlights, new GUIContent(""), customSkin.FindStyle("Toggle Helper"));
+//         GUILayout.EndHorizontal();
+//         GUILayout.Space(foldoutBottomSpace);
 
-        if (showHighlights)
-        {
-            EditorDrawer.DrawProperty(defaultHighlight, customSkin, "Default Highlight");
-            EditorDrawer.DrawProperty(interactionPositiveHighlight, customSkin, "Interaction Positive Highlight");
-            EditorDrawer.DrawProperty(interactionNegativeHighlight, customSkin, "Interaction Negative Highlight");
-            EditorDrawer.DrawProperty(targetHighlight, customSkin, "Target Highlight");
-            EditorDrawer.DrawProperty(triggerHighlight, customSkin, "Trigger Highlight");
-            EditorDrawer.DrawProperty(abilityHighlight, customSkin, "Ability Highlight");
-            EditorDrawer.DrawProperty(selectedHighlight, customSkin, "Selected Highlight");
-        }
+//         if (showHighlights)
+//         {
+//             EditorDrawer.DrawProperty(defaultHighlight, customSkin, "Default Highlight");
+//             EditorDrawer.DrawProperty(interactionPositiveHighlight, customSkin, "Interaction Positive Highlight");
+//             EditorDrawer.DrawProperty(interactionNegativeHighlight, customSkin, "Interaction Negative Highlight");
+//             EditorDrawer.DrawProperty(targetHighlight, customSkin, "Target Highlight");
+//             EditorDrawer.DrawProperty(triggerHighlight, customSkin, "Trigger Highlight");
+//             EditorDrawer.DrawProperty(abilityHighlight, customSkin, "Ability Highlight");
+//             EditorDrawer.DrawProperty(selectedHighlight, customSkin, "Selected Highlight");
+//         }
 
-        #endregion
+//         #endregion
 
 
-        GUILayout.EndVertical();
-        GUILayout.Space(foldoutItemSpace);
+//         GUILayout.EndVertical();
+//         GUILayout.Space(foldoutItemSpace);
 
-        // Apply changes
-        serializedObject.ApplyModifiedProperties();
-        Repaint();
+//         // Apply changes
+//         serializedObject.ApplyModifiedProperties();
+//         Repaint();
 
-        // Reset to defaults button
-        // GUILayout.Space(12);
-        // GUILayout.BeginHorizontal();
+//         // Reset to defaults button
+//         // GUILayout.Space(12);
+//         // GUILayout.BeginHorizontal();
 
-        if (GUILayout.Button(new GUIContent("Reset to defaults")))
-            ResetToDefaults();
+//         if (GUILayout.Button(new GUIContent("Reset to defaults")))
+//             ResetToDefaults();
 
-    }
+//     }
 
-    void ResetToDefaults()
-    {
-        if (! EditorUtility.DisplayDialog("Reset to defaults", "Are you sure you want to reset to default?", "Yes", "Cancel")) return;
+//     void ResetToDefaults()
+//     {
+//         if (! EditorUtility.DisplayDialog("Reset to defaults", "Are you sure you want to reset to default?", "Yes", "Cancel")) return;
         
-        try
-        {
-            var preset = Resources.Load<Preset>(baseResourcePath + defaultPreset);
-            preset.ApplyTo(Resources.Load(baseResourcePath + defaultColorsEditor));
-            Selection.activeObject = null;
-            Debug.Log("<b>[Sors Colors]</b> Resetting is successful.");
-        }
+//         try
+//         {
+//             var preset = Resources.Load<Preset>(baseResourcePath + defaultPreset);
+//             preset.ApplyTo(Resources.Load(baseResourcePath + defaultColorsEditor));
+//             Selection.activeObject = null;
+//             Debug.Log("<b>[Sors Colors]</b> Resetting is successful.");
+//         }
 
-        catch { Debug.LogWarning("<b>[Sors Colors]</b> Resetting failed. Default preset seems to be missing"); }
+//         catch { Debug.LogWarning("<b>[Sors Colors]</b> Resetting failed. Default preset seems to be missing"); }
         
-    }
-}
+//     }
+// }
 
-#endif
+// #endif
